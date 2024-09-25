0b6d59d38199e37886f8ada6f5aed5a66ce64de2
Ben Garrett <code.by.ben@gmail.com>
2024-09-26 00:23:09 +1000

    examples for docs.

--- string_test.go
+++ string_test.go
@@ -1,8 +1,10 @@
 package helper_test
 
 import (
+	"fmt"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/Defacto2/helper"
 	"github.com/google/uuid"
@@ -14,6 +16,170 @@ const (
 	unid = "00000000-0000-0000-0000-000000000000" // common universal unique identifier example
 	cfid = "00000000-0000-0000-0000000000000000"  // coldfusion uuid example
 )
+
+func ExampleByteCount() {
+	fmt.Println(helper.ByteCount(0))
+	fmt.Println(helper.ByteCount(1024))
+	fmt.Println(helper.ByteCount(1024 * 1024))
+	// Output:
+	// 0B
+	// 1k
+	// 1M
+}
+
+func ExampleByteCountFloat() {
+	fmt.Println(helper.ByteCountFloat(1024 * 1024 * 1024))
+	// Output:
+	// 1.1 GB
+}
+
+func ExampleCapitalize() {
+	fmt.Println(helper.Capitalize("hello world"))
+	// Output:
+	// Hello world
+}
+
+func ExampleCfUUID() {
+	newid, _ := helper.CfUUID("00000000-0000-0000-0000000000000000")
+	fmt.Println(newid)
+	// Output:
+	// 00000000-0000-0000-0000-000000000000
+}
+
+func ExampleDeleteDupe() {
+	fmt.Println(helper.DeleteDupe("a", "b", "a"))
+	// Output:
+	// [a b]
+}
+
+func ExampleFinds() {
+	fmt.Println(helper.Finds("bravo", "alfa", "bravo", "charlie", "delta"))
+	fmt.Println(helper.Finds("bravo", "alfa", "charlie", "delta"))
+	// Output:
+	// true
+	// false
+}
+
+func ExampleFmtSlice() {
+	fmt.Println(helper.FmtSlice("alfa,bravo,charlie"))
+	// Output:
+	// Alfa, Bravo, Charlie
+}
+
+func ExampleIntegrityBytes() {
+	fmt.Println(helper.IntegrityBytes([]byte("hello")))
+	// Output:
+	// sha384-WeF0h3dEjGnea4ANejO7+5/xtGPkQ1TDVTvNucZm+pASWjx5+QOXvfX2oT3oKGhP
+}
+
+func ExampleMaxLineLength() {
+	s := strings.Repeat("a", 100) + "\n" + strings.Repeat("b", 50)
+	fmt.Println(helper.MaxLineLength(s))
+	// Output:
+	// 100
+}
+
+func ExampleObfuscate() {
+	fmt.Println(helper.Obfuscate("1"))
+	fmt.Println(helper.Obfuscate("abc"))
+	// Output:
+	// 9b1c6
+	// abc
+}
+
+func ExamplePageCount() {
+	fmt.Println(helper.PageCount(1000, 100))
+	// Output:
+	// 10
+}
+
+func ExampleReleased() {
+	year, month, day := helper.Released("2024-07-15")
+	fmt.Println(year, month, day)
+	// Output:
+	// 2024 7 15
+}
+
+func ExampleReverseInt() {
+	i, _ := helper.ReverseInt(123456)
+	fmt.Println(i)
+	// Output:
+	// 654321
+}
+
+func ExampleShortMonth() {
+	fmt.Println(helper.ShortMonth(1))
+	// Output:
+	// Jan
+}
+
+func ExampleSlug() {
+	fmt.Println(helper.Slug("Hello World Homepage!"))
+	// Output:
+	// hello-world-homepage
+}
+
+func ExampleTimeDistance() {
+	oneHourAgo := time.Now().Add(-time.Hour)
+	oneHourAhead := time.Now().Add(time.Hour)
+	fmt.Println(helper.TimeDistance(oneHourAgo, time.Now(), true))
+	fmt.Println(helper.TimeDistance(oneHourAgo, oneHourAhead, true))
+	// Output:
+	// about 1 hour
+	// about 2 hours
+}
+
+func ExampleAdd1() {
+	num := 2
+	fmt.Println(helper.Add1(num))
+	// Output:
+	// 3
+}
+
+func ExampleTitleize() {
+	fmt.Println(helper.Titleize("hello world"))
+	// Output:
+	// Hello World
+}
+
+func ExampleTrimPunct() {
+	fmt.Println(helper.TrimPunct("OMG?!?"))
+	// Output:
+	// OMG
+}
+
+func ExampleTrimRoundBraket() {
+	fmt.Println(helper.TrimRoundBraket("Hello (world)"))
+	// Output:
+	// Hello
+}
+
+func ExampleYears() {
+	fmt.Println(helper.Years(1990, 2000))
+	// Output:
+	// the years 1990 - 2000
+}
+
+func ExampleDeObfuscate() {
+	fmt.Println(helper.DeObfuscate("9b1c6"))
+	// Output:
+	// 1
+}
+
+func ExampleChrLast() {
+	fmt.Println(helper.ChrLast("hello"))
+	fmt.Println(helper.ChrLast("abc\n"))
+	// Output:
+	// o
+	// c
+}
+
+func ExampleDetermine() {
+	a := strings.NewReader("hello")
+	fmt.Println(helper.Determine(a))
+	// Output:
+	// ISO 8859-1
+}
 
 func TestCfUUID(t *testing.T) {
 	t.Parallel()
